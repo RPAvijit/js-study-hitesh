90d54a5812336e0e4b87f015faebb15a5b502f2b
RPAvijit <67783149+RPAvijit@users.noreply.github.com>
2025-08-28 18:21:01 +0000

    prefix postfix

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,13 @@
-console.log("avijit")
+// console.log("avijit")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
